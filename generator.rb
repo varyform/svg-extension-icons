8c343ac41274ba6066e4e28e494027646d231541
Oleh Khomey <varyform@gmail.com>
2015-03-13 21:43:03 +0200

    Get color from MD5

--- generator.rb
+++ generator.rb
@@ -2,14 +2,14 @@ require 'color'
 require 'digest/md5'
 
 def generate(extension)
-  hash = Digest::MD5.hexdigest(extension).sum
+  color = '#' << Digest::MD5.hexdigest(extension)[0..5]
 
-  # base_hue = rand
-  base_hue = (hash & 0x0000FF) / 256.0
+  hue = Color::RGB.from_html(color).to_hsl.hue / 360.0
+  p [color, hue]
 
-  base_color    = Color::HSL.from_fraction(base_hue, 0.62, 0.63).html
-  lighten_color = Color::HSL.from_fraction(base_hue, 0.62, 0.83).html
-  darken_color  = Color::HSL.from_fraction(base_hue, 0.62, 0.53).html
+  base_color    = Color::HSL.from_fraction(hue, 0.62, 0.63).html
+  lighten_color = Color::HSL.from_fraction(hue, 0.62, 0.83).html
+  darken_color  = Color::HSL.from_fraction(hue, 0.62, 0.53).html
 
   template = <<-SVG
     <svg xmlns="http://www.w3.org/2000/svg" width="75" height="100" viewBox="0 0 70 100" enable-background="new 0 0 70 100">
@@ -25,7 +25,7 @@ def generate(extension)
   SVG
 end
 
-extensions = %w(zip txt doc png gif mp3)
+# extensions = %w(zip txt doc png gif mp3)
 extensions = %w(3gp 7z ace ai aif aiff amr asf asx bat bin bmp bup cab cbr cda cdl cdr chm dat divx dll dmg doc dss dvf dwg eml eps exe fla flv gif gz hqx htm html ifo indd iso jar jpeg jpg lnk log m4a m4b m4p m4v mcd mdb mid mov mp2 mp4 mpeg mpg msi mswmm ogg pdf png pps ps psd pst ptb pub qbb qbw qxd ram rar rm rmvb rtf sea ses sit sitx ss swf tgz thm tif tmp torrent ttf txt unknown vcd vob wav wma wmv wps xls xpi zip)
 
 result = <<-HTML
@@ -40,14 +40,6 @@ HTML
 srand
 
 icons = extensions.map do |ext|
-  hash = Digest::MD5.hexdigest(ext).sum
-
-  r = (hash & 0xFF0000) >> 16
-  g = (hash & 0x00FF00) >> 8
-  b = hash & 0x0000FF
-
-  p [r, g, b]
-
   generate(ext)
 end.join
 
